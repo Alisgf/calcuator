12872e1f500eb6b6eee8e12d061da6c2a8e5c04a
Ali <aliop68@gmali.com>
2024-08-12 15:04:56 +0300

    alment calucter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,29 @@
 </svelte:head>
 
 
-<div class="bg-white h-[40rem] w-[20rem] rounded-3xl ">
-    <button>
-        
-    </button>
+<div class="bg-white h-[35rem] w-[20rem] 
+rounded-3xl grid grid-cols-4 gap-4">
+    
+    
+    
+    <button>%</button>
+    <button>√</button>
+    <button>CE</button>
+    <button>C</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button>-</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>×</button>
+    <button>.</button>
+    <button>0</button>
+    <button>=</button>
+    <button>+</button>
 </div>
-
-
