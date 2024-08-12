ddc16dda83080a4cc7c200cea66616e6cec69d69
Ali <aliop68@gmali.com>
2024-08-13 02:12:55 +0300

    number

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
+<script lang="ts">
+    import SquareRoot from '$lib/SquareRoot.svelte';
+    import Minus from '$lib/icon/Minus.svelte';
+    import Division from'$lib/icon/Division.svelte';
+    import PlusIcon from '$lib/icon/PlusIcon.svelte';
+    import MultiplyIcon from '$lib/icon/MultiplyIcon.svelte';
+     function oddToequation(value:string) {
+        equation += value
+     }
+    let equation:string = "";
+
+</script>
+
+
+
 <svelte:head>
     <title>
         الة حاسبة
@@ -10,30 +25,44 @@ rounded-3xl grid grid-cols-4 gap-1 p-5 font-semibold text-xl shadow-xl">
 
 <div class="bg-blue-500 rounded-full col-span-4 h-16 
      flex items-center px-2 mb-2 text-white ">
-4474474
+{equation}
 
 </div>
     
     
     
-    <button class="bg-slate-200">%</button>
-    <button class="bg-slate-200">√</button>
-    <button class="bg-slate-200">CE</button>
-    <button class="bg-slate-600 text-white">C</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button class="bg-red-500 text-white">-</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button class="bg-blue-500 text-white">÷</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button class="bg-yellow-400 text-white">×</button>
-    <button>.</button>
-    <button>0</button>
-    <button class="bg-slate-200">=</button>
-    <button class="bg-green-500 text-white">+</button>
+    <button class="bg-slate-200 hover:bg-slate-200/85"on:click={()=> oddToequation ('/100')}>%</button>
+  
+    <!-- square root -->
+    <button class="bg-slate-200 hover:bg-slate-200/85"on:click={()=> oddToequation ('0')}>
+        <SquareRoot/>
+    </button>
+    <button class="bg-slate-200 hover:bg-slate-200/85"on:click={()=> oddToequation ('CE')}>CE</button>
+    <button class="bg-slate-600 hover:bg-slate-600/85 text-white" on:click={()=> oddToequation ('C')}>C</button>
+    <button on:click={()=> oddToequation ('7')}>7</button>
+    <button on:click={()=> oddToequation ('8')} >8</button>
+    <button on:click={()=> oddToequation ('9')}>9</button>
+    <!-- Minus -->
+    <button class="bg-red-500 hover:bg-red-500/85 text-white"on:click={()=> oddToequation (' - ')} >
+    <Minus/>
+    </button>
+    <button on:click={()=> oddToequation ('4')}>4</button>
+    <button on:click={()=> oddToequation ('5')} >5</button>
+    <button on:click={()=> oddToequation ('6')}>6</button>
+    <!-- division -->
+    <button class="bg-blue-500 hover:bg-blue-500/85 text-white"on:click={()=> oddToequation ('/')}>
+        <Division/>
+    </button>
+    <button on:click={()=> oddToequation ('1')}>1</button>
+    <button on:click={()=> oddToequation ('2')} >2</button>
+    <button on:click={()=> oddToequation ('3')}>3</button>
+    <button class="bg-yellow-400 hover:bg-yellow-400/85 text-white" on:click={()=> oddToequation (' * ')}>
+     <MultiplyIcon/>
+    </button>
+    <button on:click={()=> oddToequation ('.')}>.</button>
+    <button on:click={()=> oddToequation ('0')}>0</button>
+    <button class="bg-slate-200 hover:bg-slate-200/75"on:click={()=> oddToequation (' = ')}>=</button>
+    <button class="bg-green-500 hover:bg-green-500/85 text-white"on:click={()=> oddToequation (' + ')} >
+        <PlusIcon/>
+    </button>
 </div>
