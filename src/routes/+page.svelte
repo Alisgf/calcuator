aeecaef88cebc739b5c306a3da1a0791b16f5fdb
Ali <aliop68@gmali.com>
2024-08-12 20:21:03 +0300

    shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,34 +6,34 @@
 
 
 <div class="bg-white h-[29rem] w-[20rem] 
-rounded-3xl grid grid-cols-4 gap-1 p-5">
+rounded-3xl grid grid-cols-4 gap-1 p-5 font-semibold text-xl shadow-xl">
 
 <div class="bg-blue-500 rounded-full col-span-4 h-16 
-flex items-center px-3 mb-4 ">
+     flex items-center px-2 mb-2 text-white ">
 4474474
 
 </div>
     
     
     
-    <button>%</button>
-    <button>√</button>
-    <button>CE</button>
-    <button class="bg-slate-600">C</button>
+    <button class="bg-slate-200">%</button>
+    <button class="bg-slate-200">√</button>
+    <button class="bg-slate-200">CE</button>
+    <button class="bg-slate-600 text-white">C</button>
     <button>7</button>
     <button>8</button>
     <button>9</button>
-    <button class="bg-red-500">-</button>
+    <button class="bg-red-500 text-white">-</button>
     <button>4</button>
     <button>5</button>
     <button>6</button>
-    <button class="bg-blue-500">÷</button>
+    <button class="bg-blue-500 text-white">÷</button>
     <button>1</button>
     <button>2</button>
     <button>3</button>
-    <button class="bg-yellow-400">×</button>
+    <button class="bg-yellow-400 text-white">×</button>
     <button>.</button>
     <button>0</button>
     <button class="bg-slate-200">=</button>
-    <button class="bg-green-500">+</button>
+    <button class="bg-green-500 text-white">+</button>
 </div>
