cfa0915d81ef63fde74030f1c44d67e462ca3c9c
Ali <aliop68@gmali.com>
2024-08-12 15:37:22 +0300

    calcuter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,29 +5,35 @@
 </svelte:head>
 
 
-<div class="bg-white h-[35rem] w-[20rem] 
-rounded-3xl grid grid-cols-4 gap-4">
+<div class="bg-white h-[29rem] w-[20rem] 
+rounded-3xl grid grid-cols-4 gap-1 p-5">
+
+<div class="bg-blue-500 rounded-full col-span-4 h-16 
+flex items-center px-3 mb-4 ">
+4474474
+
+</div>
     
     
     
     <button>%</button>
     <button>√</button>
     <button>CE</button>
-    <button>C</button>
+    <button class="bg-slate-600">C</button>
     <button>7</button>
     <button>8</button>
     <button>9</button>
-    <button>-</button>
+    <button class="bg-red-500">-</button>
     <button>4</button>
     <button>5</button>
     <button>6</button>
-    <button>÷</button>
+    <button class="bg-blue-500">÷</button>
     <button>1</button>
     <button>2</button>
     <button>3</button>
-    <button>×</button>
+    <button class="bg-yellow-400">×</button>
     <button>.</button>
     <button>0</button>
-    <button>=</button>
-    <button>+</button>
+    <button class="bg-slate-200">=</button>
+    <button class="bg-green-500">+</button>
 </div>
